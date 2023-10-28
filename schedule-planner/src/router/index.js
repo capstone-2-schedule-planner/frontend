f455693c8ba814137cc4dcd3ad6d5b4814c381e7
NickEgithub <nicholaseedmundson@gmail.com>
2023-10-28 14:42:18 -0500

    scheduler page

--- schedule-planner/src/router/index.js
+++ schedule-planner/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
+import Scheduler from "../views/Scheduler.vue"
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home,
+    },
+    {
+        path: "/Scheduler",
+        name: "Scheduler",
+        component: Scheduler,
     }
 ];
 
---
+++ schedule-planner/src/views/Scheduler.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="scheduler">
+      <dragbox v-for="(semester, index) in semesterData" :key="index" :term="semester.term" :year="semester.year" :list-value="semester.listValue" />
+    </div>
+  </template>
+  
+  
+  <script>
+  import dragbox from '../components/dragbox.vue';
+  import { ref, computed } from 'vue';
+  
+  export default {
+    name: "Scheduler",
+    components: {
+      dragbox,
+    },
+    setup() {
+      
+      const semesterData = ref([])
+  
+      // Function to generate semester data
+      const generateSemesterData = () => {
+        const startingYear = 2024;
+        const numberOfSemesters = 8; // Adjust this as needed
+  
+        for (let i = 0; i < numberOfSemesters; i++) {
+          const year = startingYear + Math.floor(i / 2);
+          const term = i % 2 === 0 ? 'Spring' : 'Fall';
+          const listValue = i + 1;
+  
+          semesterData.value.push({
+            term,
+            year,
+            listValue,
+          })
+        }
+      };
+  
+      // Call the function to generate semester data
+      generateSemesterData();
+
+      return {
+        semesterData,
+      }
+    
+    },
+  };
+  </script>
+  
