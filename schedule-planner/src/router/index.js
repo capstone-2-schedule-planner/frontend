80f744b9fb17587f462b210a002df51b828e8f5d
Declan Ortinau <declan@ortinau.com>
2023-10-28 16:43:29 -0500

    draggable scheduler page

--- schedule-planner/src/router/index.js
+++ schedule-planner/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import Home from "../views/Home.vue"
+import Scheduler from "../views/DraggableScheduler.vue"
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: Home,
+    },
+    {
+        path: "/Scheduler",
+        name: "Scheduler",
+        component: Scheduler,
     }
 ];
 
