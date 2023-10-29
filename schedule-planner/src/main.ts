c21d614b263e31803fd7980471cac1bf0163a6c0
Declan Ortinau <declan@ortinau.com>
2023-10-29 16:15:10 -0500

    Search modal with dummy data working

--- schedule-planner/src/main.ts
+++ schedule-planner/src/main.ts
@@ -7,13 +7,35 @@ import router from './router'
 // Vuetify
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+const myCustomTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    //background: '#FFFFFF',
+    //surface: '#FFFFFF',
+    primary: '#144b34',
+    'primary-darken-1': '#144b34',
+    secondary: '#b9c8c2',
+    'secondary-darken-1': '#55766c',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+}
+
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  theme: {
+    defaultTheme: 'myCustomTheme',
+    themes: {
+      myCustomTheme,
+    }
+  }
 })
 
 createApp(App).use(router).use(vuetify).mount('#app')
